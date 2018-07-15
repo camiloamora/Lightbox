bbe20582df1b0a3f6c4bff0b830f1fe1184cf9e0
CamiloMora <kalufau@hotmail.com>
2018-07-15 18:30:28 -0500

    Linea Base con archivo de configuración para gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,50 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import pug from 'gulp-pug';
+import browserSync from 'browser-sync';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import cssnano from 'cssnano';
+
+const server = browserSync.create();
+
+const postcssPlugins = [
+  cssnano({
+    autoprefixer: {
+      add: true
+    }
+  })
+];
+
+gulp.task('es6', () =>
+    gulp.src('./dev/js/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('./public/js'))
+);
+
+gulp.task('sass', () =>
+    gulp.src('./dev/scss/styles.scss')
+      .pipe(sass())
+      .pipe(postcss(postcssPlugins))
+      .pipe(gulp.dest('./public/css'))
+      .pipe(server.stream({match: '**/*.css'}))
+);
+
+gulp.task('pug', () =>
+    gulp.src('./dev/pug/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./public'))
+);
+
+gulp.task('default', () => {
+    server.init({
+        server:{
+            baseDir: './public'
+        }
+    });
+
+    gulp.watch('./dev/js/*.js', ['es6', server.reload]);
+    gulp.watch('./dev/scss/**/*.scss', ['pug', server.reload]);
+    gulp.watch('./dev/pug/**/*.pug', ['pug', server.reload]);
+
+});
